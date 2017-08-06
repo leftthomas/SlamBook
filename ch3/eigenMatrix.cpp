cf8bc365dbc9325992a5563271ee73b94ce2927f
left thomas <leftthomas@qq.com>
2017-08-06 16:04:47 +0800

    添加Eigen使用初始化代码

---
+++ ch3/eigenMatrix.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+//Eigen部分
+#include <Eigen/Core>
+//稠密矩阵的代数运算（逆，特征值等）
+
+
+#define MATRIX_SIZE 50
+
+/*
+ * 本程序演示了Eigen基本类型的使用
+ */
+
+int main(int argc, char **argv) {
+    cout << "Hello SLAM!" << endl;
+    return 0;
+}
