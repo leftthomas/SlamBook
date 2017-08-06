e8bdafcac8ed7f886deb16a6b6c32d586f5f663b
left thomas <leftthomas@qq.com>
2017-08-06 16:36:50 +0800

    完成矩阵元素访问，矩阵向量乘法

--- ch3/eigenMatrix.cpp
+++ ch3/eigenMatrix.cpp
@@ -32,5 +32,19 @@ int main(int argc, char **argv) {
     //输出
     cout << matrix_23 << endl;
 
+    //访问矩阵元素
+    for (int i = 0; i < 1; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << matrix_23(i, j) << endl;
+        }
+    }
+
+    v_3d << 3, 2, 1;
+    //矩阵和向量相乘，注意类型转换以及运算结果的维数
+    Eigen::Matrix<double, 2, 1> result = matrix_23.cast<double>() * v_3d;
+    cout << result << endl;
+
+    matrix_33 = Eigen::Matrix3d::Random();
+    cout << matrix_33 << endl;
     return 0;
 }
