e19475ef6d9e79dd8c43f86dcdc9dbb9882061ca
left thomas <leftthomas@qq.com>
2017-08-18 14:32:13 +0800

    代价函数的计算模型

--- ch6/useCeres.cpp
+++ ch6/useCeres.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
+#include <ceres/ceres.h>
 
 using namespace std;
+
+//代价函数的计算模型
+struct CURVE_FITTING_COST {
+//    x,y数据
+    const double _x, _y;
+
+    CURVE_FITTING_COST(double x, double y) : _x(x), _y(y) {}
+
+//    残差的计算
+    template<typename T>
+    bool operator()(const T *const abc,//模型参数，有三维
+                    T *residual) const//残差
+    {
+//        y-exp(ax^2+bx+c)
+        residual[0] = T(_y) - ceres::exp(abc[0] * T(_x) * T(_x) + abc[1] * T(_x) + abc[2]);
+        return true;
+    }
+};
 
 /**
  * 本程序演示了Ceres的使用
