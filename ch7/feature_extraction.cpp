0ae8384dff37fc2b54124d9d66b07526d09110bf
left thomas <leftthomas@qq.com>
2017-08-22 16:45:02 +0800

    1、图像读取初始化； 2、特征点初始化

--- ch7/feature_extraction.cpp
+++ ch7/feature_extraction.cpp
@@ -1,15 +1,34 @@
 #include<iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 using namespace std;
-
+using namespace cv;
 
 /**
- * 本程序演示了Eigen基本类型的使用
+ * 本程序演示了ORB特征提取与匹配
  * @param argc
  * @param argv
  * @return
  */
 int main(int argc, char **argv) {
+
+//    判断命令行参数是否有给出两张图片路径
+    if (argc != 3) {
+        cout << "usage: feature_extraction img1 img2" << endl;
+        return 1;
+    }
+
+//    读取图像
+    Mat img_1 = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    Mat img_2 = imread(argv[2], CV_LOAD_IMAGE_COLOR);
+
+//    初始化关键点集、描述子、ORB特征
+    vector<KeyPoint> keypoints_1, keypoints_2;
+    Mat descriptors_1, descriptors_2;
+//    采用默认参数
+    Ptr<ORB> orb = ORB::create(500, 1.2f, 8, 31, 0, 2, ORB::HARRIS_SCORE, 31, 20);
 
     return 0;
 }
