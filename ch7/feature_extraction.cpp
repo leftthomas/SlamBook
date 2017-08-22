c3a4e29d9aaf47fd50d6480753d58c94d89bf730
left thomas <leftthomas@qq.com>
2017-08-22 17:06:29 +0800

    匹配描述子，点对筛选,找出最相似的和最不相似的两组点之间的距离

--- ch7/feature_extraction.cpp
+++ ch7/feature_extraction.cpp
@@ -25,10 +25,41 @@ int main(int argc, char **argv) {
     Mat img_2 = imread(argv[2], CV_LOAD_IMAGE_COLOR);
 
 //    初始化关键点集、描述子、ORB特征
-    vector<KeyPoint> keypoints_1, keypoints_2;
+    vector<KeyPoint> key_points_1, key_points_2;
     Mat descriptors_1, descriptors_2;
 //    采用默认参数
-    Ptr<ORB> orb = ORB::create(500, 1.2f, 8, 31, 0, 2, ORB::HARRIS_SCORE, 31, 20);
+    Ptr<ORB> orb = ORB::create();
+
+//    第一步：检测 oriented FAST角点位置
+    orb->detect(img_1, key_points_1);
+    orb->detect(img_2, key_points_2);
+
+//    第二步：计算BRIEF描述子
+    orb->compute(img_1, key_points_1, descriptors_1);
+    orb->compute(img_2, key_points_2, descriptors_2);
+
+//    绘制ORB特征点
+    Mat out_img_1;
+    drawKeypoints(img_1, key_points_1, out_img_1);
+    imshow("ORB特征点", out_img_1);
+    waitKey(0);
+
+//    第三步：匹配描述子，使用Hamming距离
+    vector<DMatch> matches;
+    BFMatcher matcher(NORM_HAMMING);
+    matcher.match(descriptors_1, descriptors_2, matches, noArray());
+
+//    第四步：匹配点对筛选,找出最相似的和最不相似的两组点之间的距离
+    double min_dist = 10000, max_dist = 0;
+    for (int i = 0; i < descriptors_1.rows; ++i) {
+        double dist = matches[i].distance;
+        if (dist < min_dist) min_dist = dist;
+        if (dist > max_dist) max_dist = dist;
+    }
+
+    printf("-- max dist : %f \n", max_dist);
+    printf("-- min dist : %f \n", min_dist);
+
 
     return 0;
 }
