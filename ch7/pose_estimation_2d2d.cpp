ee339a30e43d6c4ec8242642bb0d24a8d2c7cbf2
left thomas <leftthomas@qq.com>
2017-08-23 20:31:18 +0800

    完成三角测量

--- ch7/pose_estimation_2d2d.cpp
+++ ch7/pose_estimation_2d2d.cpp
@@ -80,7 +80,7 @@ void pose_estimation_2d2d(vector<KeyPoint> key_points_1, vector<KeyPoint> key_po
 //    焦距,TUM dataset标定值
     int focal_length = 521;
     Mat essential_matrix;
-    essential_matrix = findEssentialMat(points_1, points_2, focal_length, principal_point, RANSAC);
+    essential_matrix = findEssentialMat(points_1, points_2, focal_length, principal_point);
     cout << "essential matrix is \n" << essential_matrix << endl;
 
 //    计算单应矩阵
@@ -93,6 +93,7 @@ void pose_estimation_2d2d(vector<KeyPoint> key_points_1, vector<KeyPoint> key_po
     cout << "R is \n" << R << endl;
     cout << "t is \n" << t << endl;
 };
+
 
 /**
  * 根据相机内参矩阵将像素坐标转换为相机坐标
@@ -108,8 +109,50 @@ Point2d pixel2cam(const Point2d &p, const Mat &K) {
             );
 }
 
+
 /**
- * 本程序演示了对极约束求解相机运动
+ * 三角测量
+ * @param key_points_1
+ * @param key_points_2
+ * @param matches
+ * @param R
+ * @param t
+ * @param points
+ */
+void triangulation(const vector<KeyPoint> &key_points_1, const vector<KeyPoint> &key_points_2,
+                   const vector<DMatch> &matches, const Mat &R, const Mat &t, vector<Point3d> &points) {
+    Mat_<float> T1(3, 4);
+    T1 << 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0;
+    Mat_<float> T2(3, 4);
+    T2 << R.at<double>(0, 0), R.at<double>(0, 1), R.at<double>(0, 2), t.at<double>(0, 0),
+            R.at<double>(1, 0), R.at<double>(1, 1), R.at<double>(1, 2), t.at<double>(1, 0),
+            R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2), t.at<double>(2, 0);
+
+//        将像素坐标转换为相机坐标
+    Mat_<double> K(3, 3);
+    K << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1;
+    vector<Point2d> pts_1, pts_2;
+    for (DMatch m : matches) {
+        pts_1.push_back(pixel2cam(key_points_1[m.queryIdx].pt, K));
+        pts_2.push_back(pixel2cam(key_points_2[m.trainIdx].pt, K));
+    }
+
+//    三角测量
+    Mat pts_4d;
+    triangulatePoints(T1, T2, pts_1, pts_2, pts_4d);
+
+//    转换成非齐次坐标
+    for (int i = 0; i < pts_4d.cols; ++i) {
+        Mat x = pts_4d.col(i);
+//        归一化
+        x /= x.at<float>(3, 0);
+        Point3d p(x.at<float>(0, 0), x.at<float>(1, 0), x.at<float>(2, 0));
+        points.push_back(p);
+    }
+}
+
+/**
+ * 本程序演示了对极约束求解相机运动以及三角测量
  * @param argc
  * @param argv
  * @return
@@ -134,14 +177,20 @@ int main(int argc, char **argv) {
 //    估计图像间的运动
     Mat R, t;
     pose_estimation_2d2d(key_points_1, key_points_2, matches, R, t);
+
 //    验证E=t^R*scale
     Mat1d t_x(3, 3);
     t_x << 0, -t.at<double>(2, 0), t.at<double>(1, 0), t.at<double>(2, 0), 0, -t.at<double>(0, 0),
             -t.at<double>(1, 0), t.at<double>(0, 0), 0;
     cout << "t^R=\n" << t_x * R << endl;
-//    验证对极约束
-    Mat1d K(3, 3);
+
+//    三角化
+    vector<Point3d> points;
+    triangulation(key_points_1, key_points_2, matches, R, t, points);
+
+    Mat_<double> K(3, 3);
     K << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1;
+//    验证对极约束
     for (DMatch m : matches) {
         Point2d pt1 = pixel2cam(key_points_1[m.queryIdx].pt, K);
         Mat1d y1(3, 1);
@@ -151,6 +200,21 @@ int main(int argc, char **argv) {
         y2 << pt2.x, pt2.y, 1;
         Mat d = y2.t() * t_x * R * y1;
         cout << "epipolar constraint = " << d << endl;
+    }
+//    验证三角化点与特征点的重投影关系
+    for (int i = 0; i < matches.size(); ++i) {
+//        第一幅图
+        Point2d pt1_cam = pixel2cam(key_points_1[matches[i].queryIdx].pt, K);
+        Point2d pt1_cam_3d(points[i].x / points[i].z, points[i].y / points[i].z);
+        cout << "point in the first camera frame: " << pt1_cam << endl;
+        cout << "point projected from 3D " << pt1_cam_3d << ", d=" << points[i].z << endl;
+//        第二幅图
+        Point2f pt2_cam = pixel2cam(key_points_2[matches[i].trainIdx].pt, K);
+        Mat pt2_trans = R * (Mat_<double>(3, 1) << points[i].x, points[i].y, points[i].z) + t;
+        pt2_trans /= pt2_trans.at<double>(2, 0);
+        cout << "point in the second camera frame: " << pt2_cam << endl;
+        cout << "point reprojected from second frame " << pt2_trans.t() << endl;
+        cout << endl;
     }
     return 0;
 }
