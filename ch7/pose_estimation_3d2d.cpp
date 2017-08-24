6168ed1ec671a4dbd26bdc12b405004f7051d41a
left thomas <leftthomas@qq.com>
2017-08-24 14:17:10 +0800

    完成Bundle Adjustment

--- ch7/pose_estimation_3d2d.cpp
+++ ch7/pose_estimation_3d2d.cpp
@@ -9,6 +9,7 @@
 #include <g2o/core/block_solver.h>
 #include <g2o/core/optimization_algorithm_levenberg.h>
 #include <g2o/solvers/csparse/linear_solver_csparse.h>
+#include <g2o/types/sba/types_six_dof_expmap.h>
 using namespace std;
 using namespace cv;
 
@@ -72,6 +73,8 @@ int main(int argc, char **argv) {
     cv::Rodrigues(r, R);
     cout << "R=\n" << R << endl;
     cout << "t=\n" << t << endl;
+//    BA
+    bundleAdjustment(pts_3d, pts_2d, K, R, t);
     return 0;
 }
 
@@ -132,4 +135,51 @@ void bundleAdjustment(const vector<Point3f> points_3d, const vector<Point2f> poi
     g2o::SparseOptimizer optimizer;
     optimizer.setAlgorithm(solver);
 
+//    vertex
+    g2o::VertexSE3Expmap *pose = new g2o::VertexSE3Expmap();
+    Eigen::Matrix3d R_mat;
+    R_mat << R.at<double>(0, 0), R.at<double>(0, 1), R.at<double>(0, 2),
+            R.at<double>(1, 0), R.at<double>(1, 1), R.at<double>(1, 2),
+            R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2);
+    pose->setId(0);
+    pose->setEstimate(g2o::SE3Quat(R_mat, Eigen::Vector3d(t.at<double>(0, 0), t.at<double>(1, 0), t.at<double>(2, 0))));
+    optimizer.addVertex(pose);
+//    landmarks
+    int index = 1;
+    for (const Point3f p:points_3d) {
+        g2o::VertexSBAPointXYZ *point = new g2o::VertexSBAPointXYZ();
+        point->setId(index++);
+        point->setEstimate(Eigen::Vector3d(p.x, p.y, p.z));
+        point->setMarginalized(true);
+        optimizer.addVertex(point);
+    }
+
+//    parameter: camera intrinsics
+    g2o::CameraParameters *camera = new g2o::CameraParameters(
+            K.at<double>(0, 0), Eigen::Vector2d(K.at<double>(0, 2), K.at<double>(1, 2)), 0);
+    camera->setId(0);
+    optimizer.addParameter(camera);
+
+//    edges
+    index = 1;
+    for (const Point2f p:points_2d) {
+        g2o::EdgeProjectXYZ2UV *edge = new g2o::EdgeProjectXYZ2UV();
+        edge->setId(index);
+        edge->setVertex(0, dynamic_cast<g2o::VertexSBAPointXYZ *>(optimizer.vertex(index)));
+        edge->setVertex(1, pose);
+        edge->setMeasurement(Eigen::Vector2d(p.x, p.y));
+        edge->setParameterId(0, 0);
+        edge->setInformation(Eigen::Matrix2d::Identity());
+        optimizer.addEdge(edge);
+        index++;
+    }
+
+    chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+    optimizer.setVerbose(true);
+    optimizer.initializeOptimization();
+    optimizer.optimize(100);
+    chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+    chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double >>(t2 - t1);
+    cout << "optimization costs time: " << time_used.count() << " seconds." << endl;
+    cout << "\nafter optimization:\n" << "T=\n" << Eigen::Isometry3d(pose->estimate()).matrix() << endl;
 }
