aed14fa94e2e437e6882972dbcd00f1dd99b2b1e
left thomas <leftthomas@qq.com>
2017-08-24 21:19:07 +0800

    PnP代码简化

--- ch7/pose_estimation_3d2d.cpp
+++ ch7/pose_estimation_3d2d.cpp
@@ -1,8 +1,5 @@
 #include<iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
+#include "common.h"
 #include <Eigen/Core>
 #include <g2o/core/base_vertex.h>
 #include <g2o/core/base_unary_edge.h>
@@ -10,18 +7,7 @@
 #include <g2o/core/optimization_algorithm_levenberg.h>
 #include <g2o/solvers/csparse/linear_solver_csparse.h>
 #include <g2o/types/sba/types_six_dof_expmap.h>
-using namespace std;
-using namespace cv;
 
-// 找到图像间的特征点匹配
-void find_feature_matches(
-        const Mat &img_1, const Mat &img_2,
-        std::vector<KeyPoint> &key_points_1,
-        std::vector<KeyPoint> &key_points_2,
-        std::vector<DMatch> &matches);
-
-// 像素坐标转相机归一化坐标
-Point2d pixel2cam(const Point2d &p, const Mat &K);
 
 // BA求解
 void bundleAdjustment(vector<Point3f> points_3d, vector<Point2f> points_2d, const Mat &K, Mat &R, Mat &t);
@@ -78,53 +64,15 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void find_feature_matches(const Mat &img_1, const Mat &img_2,
-                          std::vector<KeyPoint> &key_points_1,
-                          std::vector<KeyPoint> &key_points_2,
-                          std::vector<DMatch> &matches) {
-    //-- 初始化
-    Mat descriptors_1, descriptors_2;
-    Ptr<FeatureDetector> detector = ORB::create();
-    Ptr<DescriptorExtractor> descriptor = ORB::create();
-    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
-    //-- 第一步:检测 Oriented FAST 角点位置
-    detector->detect(img_1, key_points_1);
-    detector->detect(img_2, key_points_2);
 
-    //-- 第二步:根据角点位置计算 BRIEF 描述子
-    descriptor->compute(img_1, key_points_1, descriptors_1);
-    descriptor->compute(img_2, key_points_2, descriptors_2);
-
-    //-- 第三步:对两幅图像中的BRIEF描述子进行匹配，使用 Hamming 距离
-    vector<DMatch> match;
-    matcher->match(descriptors_1, descriptors_2, match, noArray());
-
-    //-- 第四步:匹配点对筛选
-    double min_dist = 10000, max_dist = 0;
-
-    //找出所有匹配之间的最小距离和最大距离, 即是最相似的和最不相似的两组点之间的距离
-    for (int i = 0; i < descriptors_1.rows; i++) {
-        double dist = match[i].distance;
-        if (dist < min_dist) min_dist = dist;
-        if (dist > max_dist) max_dist = dist;
-    }
-
-    //当描述子之间的距离大于两倍的最小距离时,即认为匹配有误.但有时候最小距离会非常小,设置一个经验值30作为下限.
-    for (int i = 0; i < descriptors_1.rows; i++) {
-        if (match[i].distance <= max(2 * min_dist, 30.0)) {
-            matches.push_back(match[i]);
-        }
-    }
-}
-
-Point2d pixel2cam(const Point2d &p, const Mat &K) {
-    return Point2d
-            (
-                    (p.x - K.at<double>(0, 2)) / K.at<double>(0, 0),
-                    (p.y - K.at<double>(1, 2)) / K.at<double>(1, 1)
-            );
-}
-
+/**
+ * BA求解
+ * @param points_3d
+ * @param points_2d
+ * @param K
+ * @param R
+ * @param t
+ */
 void bundleAdjustment(vector<Point3f> points_3d, vector<Point2f> points_2d,
                       const Mat &K, Mat &R, Mat &t) {
 //    初始化g2o,pose维度为6,landmark维度为3
