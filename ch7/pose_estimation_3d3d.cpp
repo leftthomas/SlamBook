e83b107958b64b02be2d839c07ed860c8e07bbb8
left thomas <leftthomas@qq.com>
2017-08-24 23:21:03 +0800

    自定义3D-3D的边

--- ch7/pose_estimation_3d3d.cpp
+++ ch7/pose_estimation_3d3d.cpp
@@ -2,14 +2,65 @@
 #include "common.h"
 #include <Eigen/Core>
 #include <Eigen/Geometry>
+#include <g2o/core/base_unary_edge.h>
 #include <g2o/core/base_vertex.h>
 #include <g2o/core/block_solver.h>
+#include <g2o/types/sba/types_six_dof_expmap.h>
 #include <g2o/solvers/eigen/linear_solver_eigen.h>
 
 
 // ICP姿态评估
 void pose_estimation_3d3d(const vector<Point3f> &pts_1, const vector<Point3f> &pts_2, Mat &R, Mat &t);
 
+// 自定义3D-3D的边
+class EdgeProjectXYZRGBDPoseOnly : public g2o::BaseUnaryEdge<3, Eigen::Vector3d, g2o::VertexSE3Expmap> {
+protected:
+    Eigen::Vector3d _point;
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+
+    EdgeProjectXYZRGBDPoseOnly(const Eigen::Vector3d &point) : _point(point) {}
+
+    virtual void computeError() {
+        const g2o::VertexSE3Expmap *pose = static_cast<const g2o::VertexSE3Expmap *>(_vertices[0]);
+//        measurement is p, point is p'
+        _error = _measurement - pose->estimate().map(_point);
+    }
+
+    virtual void linearizeOplus() {
+        g2o::VertexSE3Expmap *pose = static_cast<g2o::VertexSE3Expmap *>(_vertices[0]);
+        g2o::SE3Quat T(pose->estimate());
+        Eigen::Vector3d xyz_trans = T.map(_point);
+        double x = xyz_trans[0];
+        double y = xyz_trans[1];
+        double z = xyz_trans[2];
+
+        _jacobianOplusXi(0, 0) = 0;
+        _jacobianOplusXi(0, 1) = -z;
+        _jacobianOplusXi(0, 2) = y;
+        _jacobianOplusXi(0, 3) = -1;
+        _jacobianOplusXi(0, 4) = 0;
+        _jacobianOplusXi(0, 5) = 0;
+
+        _jacobianOplusXi(1, 0) = z;
+        _jacobianOplusXi(1, 1) = 0;
+        _jacobianOplusXi(1, 2) = -x;
+        _jacobianOplusXi(1, 3) = 0;
+        _jacobianOplusXi(1, 4) = -1;
+        _jacobianOplusXi(1, 5) = 0;
+
+        _jacobianOplusXi(2, 0) = -y;
+        _jacobianOplusXi(2, 1) = x;
+        _jacobianOplusXi(2, 2) = 0;
+        _jacobianOplusXi(2, 3) = 0;
+        _jacobianOplusXi(2, 4) = 0;
+        _jacobianOplusXi(2, 5) = -1;
+    }
+
+    bool read(istream &in) {}
+
+    bool write(ostream &out) const {}
+};
 
 /**
  * 本程序演示了ICP求解相机位姿
