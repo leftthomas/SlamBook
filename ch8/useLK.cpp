9809b566bec0cbb7b239d380c8cc52a638ad0e59
left thomas <leftthomas@qq.com>
2017-08-25 11:17:31 +0800

    LK光流法初始化

---
+++ ch8/useLK.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+
+/**
+ * 本程序演示了LK光流法
+ * data文件去TUM网站下载，注意下载associate.py之后需要修改以下两条语句：
+ * first_keys = first_list.keys() ————————> first_keys = list(first_list.keys())
+ * second_keys = second_list.keys() ————————> second_keys = list(second_list.keys())
+ * 然后运行 python associate.py rgb.txt depth.txt > associate.txt 生成 associate.txt 文件
+ * 否则在Python3下运行会报错
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char **argv) {
+
+    return 0;
+}
