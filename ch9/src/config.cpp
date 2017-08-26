063ce8f3a113d5786e3b215c70408702d103ffd6
left thomas <leftthomas@qq.com>
2017-08-26 22:03:49 +0800

    完成Config类

--- ch9/src/config.cpp
+++ ch9/src/config.cpp
@@ -6,13 +6,20 @@
 
 namespace myslam {
 
-    Config::Config() {}
+    shared_ptr<Config> Config::config_ = nullptr;
 
     Config::~Config() {
-
+        if (file_.isOpened())
+            file_.release();
     }
 
     void Config::setParameterFile(const string &filename) {
-
+        if (config_ == nullptr)
+            config_ = shared_ptr<Config>(new Config);
+        config_->file_ = cv::FileStorage(filename.c_str(), cv::FileStorage::READ);
+        if (!config_->file_.isOpened()) {
+            cerr << "parameter file " << "does not exist." << endl;
+            config_->file_.release();
+        }
     }
 }
