d96ba04ffd24dff52d811aba5e3ef88f5bef24ff
left thomas <leftthomas@qq.com>
2017-08-26 21:11:36 +0800

    完成MapPoint类

--- ch9/src/mappoint.cpp
+++ ch9/src/mappoint.cpp
@@ -2,18 +2,18 @@
 // Created by Left Thomas on 2017/8/26.
 //
 
-#include <Eigen/src/Core/Matrix.h>
-#include <opencv2/core/mat.hpp>
-#include <myslam/mappoint.h>
+#include "myslam/mappoint.h"
 
 namespace myslam {
 
-    MapPoint::MapPoint() {}
+    MapPoint::MapPoint() : id_(static_cast<unsigned long>(-1)), pos_(Vector3d(0, 0, 0)),
+                           norm_(Vector3d(0, 0, 0)), observed_times_(0), correct_times_(0) {}
 
-    MapPoint::MapPoint(unsigned long id_, const Eigen::Matrix<double, 3, 1> &pos_,
-                       const Eigen::Matrix<double, 3, 1> &norm_) : id_(id_), pos_(pos_), norm_(norm_) {}
+    MapPoint::MapPoint(unsigned long id_, const Eigen::Matrix<double, 3, 1> &pos_, const Eigen::Matrix
+            <double, 3, 1> &norm_) : id_(id_), pos_(pos_), norm_(norm_), observed_times_(0), correct_times_(0) {}
 
     MapPoint::Ptr MapPoint::createMapPoint() {
-        return myslam::MapPoint::Ptr();
+        static unsigned long factory_id = 0;
+        return MapPoint::Ptr(new MapPoint(factory_id++, Vector3d(0, 0, 0), Vector3d(0, 0, 0)));
     }
 }
