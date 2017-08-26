983a172e25eeb23abd6cce3b1823a7c5d2cb9331
left thomas <leftthomas@qq.com>
2017-08-26 21:21:08 +0800

    定义Map类

--- ch9/include/myslam/map.h
+++ ch9/include/myslam/map.h
@@ -5,9 +5,24 @@
 #ifndef SLAMBOOK_MAP_H
 #define SLAMBOOK_MAP_H
 
+#include "myslam/common_include.h"
+#include "myslam/mappoint.h"
+#include "myslam/frame.h"
+
 namespace myslam {
     class Map {
+    public:
+        typedef shared_ptr<Map> Ptr;
+//        all landmarks
+        unordered_map<unsigned long, MapPoint::Ptr> map_points_;
+//        all key frames
+        unordered_map<unsigned long, Frame::Ptr> key_frames_;
 
+        Map();
+
+        void insertKeyFrame(Frame::Ptr frame);
+
+        void insertMapPoint(MapPoint::Ptr map_points);
     };
 }
 
--- ch9/src/map.cpp
+++ ch9/src/map.cpp
@@ -2,6 +2,17 @@
 // Created by Left Thomas on 2017/8/26.
 //
 
+#include "myslam/map.h"
+
 namespace myslam {
 
+    Map::Map() {}
+
+    void Map::insertKeyFrame(Frame::Ptr frame) {
+
+    }
+
+    void Map::insertMapPoint(MapPoint::Ptr map_points) {
+
+    }
 }
