812c4364aa8cbee70ebb7e6d29813cd8e94d104f
left thomas <leftthomas@qq.com>
2017-08-26 21:44:51 +0800

    完成Config类

--- ch9/include/myslam/config.h
+++ ch9/include/myslam/config.h
@@ -5,13 +5,30 @@
 #ifndef SLAMBOOK_CONFIG_H
 #define SLAMBOOK_CONFIG_H
 
+#include "myslam/common_include.h"
 
 namespace myslam {
     class Config {
+    private:
+        static shared_ptr<Config> config_;
+        cv::FileStorage file_;
 
+//        private constructor makes it as a singleton
+        Config();
+
+    public:
+//        close the file when deconstructing
+        virtual ~Config();
+
+//        set a new config file
+        static void setParameterFile(const string &filename);
+
+//        access the parameter values
+        template<typename T>
+        static T get(const string &key) {
+            return T(Config::config_->file_[key]);
+        }
     };
 }
-
-
 
 #endif //SLAMBOOK_CONFIG_H
--- ch9/src/config.cpp
+++ ch9/src/config.cpp
@@ -2,6 +2,17 @@
 // Created by Left Thomas on 2017/8/26.
 //
 
+#include "myslam/config.h"
+
 namespace myslam {
 
+    Config::Config() {}
+
+    Config::~Config() {
+
+    }
+
+    void Config::setParameterFile(const string &filename) {
+
+    }
 }
