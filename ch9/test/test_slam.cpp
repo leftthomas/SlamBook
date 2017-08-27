49bee00f818c142d908f191198b92990ef838d00
left thomas <leftthomas@qq.com>
2017-08-27 16:22:12 +0800

    测试vo文件读取部分完成

--- ch9/test/test_slam.cpp
+++ ch9/test/test_slam.cpp
@@ -4,10 +4,41 @@
 // 手动编译安装OpenCV3，不要通过brew install opencv安装，否则是没有viz模块的
 //
 #include<iostream>
+#include <fstream>
+#include <myslam/config.h>
+#include <myslam/visual_odometry.h>
 
 using namespace std;
 
-
 int main(int argc, char **argv) {
+    if (argc != 2) {
+        cout << "usage: test_slam parameter_file" << endl;
+        return 1;
+    }
+    myslam::Config::setParameterFile(argv[1]);
+    myslam::VisualOdometry::Ptr vo(new myslam::VisualOdometry);
+
+    string dataset_dir = myslam::Config::get<string>("dataset_dir");
+    cout << "dataset: " << dataset_dir << endl;
+    ifstream fin(dataset_dir + "/associate.txt");
+    if (!fin) {
+        cout << "please generate the associate file called associate.txt!" << endl;
+        return 1;
+    }
+
+    vector<string> rgb_files, depth_files;
+    vector<double> rgb_times, depth_times;
+    while (!fin.eof()) {
+        string rgb_time, rgb_file, depth_time, depth_file;
+        fin >> rgb_time >> rgb_file >> depth_time >> depth_file;
+        rgb_times.push_back(atof(rgb_time.c_str()));
+        depth_times.push_back(atof(depth_time.c_str()));
+        rgb_files.push_back(dataset_dir + "/" + rgb_file);
+        depth_files.push_back(dataset_dir + "/" + depth_file);
+        if (!fin.good())
+            break;
+    }
+
+    myslam::Camera::Ptr camera(new myslam::Camera);
     return 0;
 }
