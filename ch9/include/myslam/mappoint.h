6bcfc3c02b252f914ac1b125b6a029acaec93f8e
left thomas <leftthomas@qq.com>
2017-08-28 10:53:11 +0800

    完成MapPoint定义

--- ch9/include/myslam/mappoint.h
+++ ch9/include/myslam/mappoint.h
@@ -6,12 +6,16 @@
 #define SLAMBOOK_MAPPOINT_H
 
 #include "myslam/common_include.h"
+#include "myslam/frame.h"
 
 namespace myslam {
     class MapPoint {
     public:
         typedef shared_ptr<MapPoint> Ptr;
         unsigned long id_;
+        static unsigned long factory_id_;
+//        whether a good point
+        bool good_;
         Vector3d pos_;
 //        Normal of viewing direction
         Vector3d norm_;
@@ -19,13 +23,26 @@ namespace myslam {
         int observed_times_;
 //        being an inliner in pose estimation
         int correct_times_;
+//        key frames that can observe this point
+        list<Frame *> observed_frames_;
+//        being an inliner in pose estimation
+        int matched_times_;
+//        being visible in current frame
+        int visible_times_;
 
         MapPoint();
 
-        MapPoint(unsigned long id_, const Vector3d &pos_, const Vector3d &norm_);
+        MapPoint(unsigned long id, const Vector3d &position, const Vector3d &norm,
+                 Frame *frame = nullptr, const Mat &descriptor = Mat());
 
+        inline cv::Point3f getPositionCV() const {
+            return cv::Point3f(pos_(0, 0), pos_(1, 0), pos_(2, 0));
+        }
 //        factory function
         static MapPoint::Ptr createMapPoint();
+
+        static MapPoint::Ptr createMapPoint(const Vector3d &pos_world, const Vector3d &norm,
+                                            const Mat &descriptor, Frame *frame);
     };
 }
 
--- ch9/src/mappoint.cpp
+++ ch9/src/mappoint.cpp
@@ -9,11 +9,18 @@ namespace myslam {
     MapPoint::MapPoint() : id_(static_cast<unsigned long>(-1)), pos_(Vector3d(0, 0, 0)),
                            norm_(Vector3d(0, 0, 0)), observed_times_(0), correct_times_(0) {}
 
-    MapPoint::MapPoint(unsigned long id_, const Eigen::Matrix<double, 3, 1> &pos_, const Eigen::Matrix
-            <double, 3, 1> &norm_) : id_(id_), pos_(pos_), norm_(norm_), observed_times_(0), correct_times_(0) {}
+    MapPoint::MapPoint(unsigned long id, const Vector3d &position, const Vector3d &norm, Frame *frame,
+                       const Mat &descriptor) {
+
+    }
 
     MapPoint::Ptr MapPoint::createMapPoint() {
-        static unsigned long factory_id = 0;
-        return MapPoint::Ptr(new MapPoint(factory_id++, Vector3d(0, 0, 0), Vector3d(0, 0, 0)));
+        return myslam::MapPoint::Ptr();
     }
+
+    MapPoint::Ptr
+    MapPoint::createMapPoint(const Vector3d &pos_world, const Vector3d &norm, const Mat &descriptor, Frame *frame) {
+        return myslam::MapPoint::Ptr();
+    }
+
 }
