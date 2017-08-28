6380bdff31c70d2f27b1fdf121293717dbc485b2
left thomas <leftthomas@qq.com>
2017-08-28 15:39:31 +0800

    添加ch10

---
+++ ch10/useSophus.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#include "sophus/so3.h"
+#include "sophus/se3.h"
+
+
+/**
+ * 本程序演示了后端
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char **argv) {
+
+    return 0;
+}
