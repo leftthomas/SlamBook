ae0c64798dcb9eae66f0b6a50e01b5719e614135
left thomas <leftthomas@qq.com>
2017-08-30 19:27:26 +0800

    pose graph优化初始化

---
+++ ch11/pose_graph_lie_algebra.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+/**
+ * 本程序演示了g2o pose graph lie algebra优化
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char **argv) {
+
+    return 0;
+}
