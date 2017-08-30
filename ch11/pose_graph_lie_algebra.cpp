cc364987048642f7a8d7118bf950ea612c5a3f3f
left thomas <leftthomas@qq.com>
2017-08-30 20:58:45 +0800

    EdgeSE3LieAlgebra定义

--- ch11/pose_graph_lie_algebra.cpp
+++ ch11/pose_graph_lie_algebra.cpp
@@ -2,7 +2,7 @@
 #include <Eigen/Core>
 #include <fstream>
 #include <g2o/core/base_vertex.h>
-#include <g2o/core/base_unary_edge.h>
+#include <g2o/core/base_binary_edge.h>
 #include <g2o/core/block_solver.h>
 #include <g2o/core/optimization_algorithm_levenberg.h>
 #include <g2o/core/optimization_algorithm_gauss_newton.h>
@@ -37,8 +37,8 @@ public:
 
     bool read(std::istream &is) override {
         double data[7];
-        for (int i = 0; i < 7; ++i) {
-            is >> data[i];
+        for (double &i : data) {
+            is >> i;
         }
         setEstimate(SE3(Eigen::Quaterniond(data[6], data[3], data[4], data[5]),
                         Eigen::Vector3d(data[0], data[1], data[2])));
@@ -62,6 +62,64 @@ protected:
 
     void setToOriginImpl() override {
         _estimate = Sophus::SE3();
+    }
+};
+
+//两个李代数节点之边
+class EdgeSE3LieAlgebra : public g2o::BaseBinaryEdge<6, SE3, VertexSE3LieAlgebra, VertexSE3LieAlgebra> {
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    void computeError() override {
+        Sophus::SE3 v1 = (dynamic_cast<VertexSE3LieAlgebra *>(_vertices[0]))->estimate();
+        Sophus::SE3 v2 = (dynamic_cast<VertexSE3LieAlgebra *>(_vertices[1]))->estimate();
+        _error = (_measurement.inverse() * v1.inverse() * v2).log();
+    }
+
+//    雅可比计算
+    void linearizeOplus() override {
+        Sophus::SE3 v1 = (dynamic_cast<VertexSE3LieAlgebra *>(_vertices[0]))->estimate();
+        Sophus::SE3 v2 = (dynamic_cast<VertexSE3LieAlgebra *>(_vertices[1]))->estimate();
+        Matrix6d J = JRInv(SE3::exp(_error));
+//        尝试把J近似为I
+        _jacobianOplusXi = -J * v2.inverse().Adj();
+        _jacobianOplusXj = J * v2.inverse().Adj();
+    }
+
+    bool read(std::istream &is) override {
+        double data[7];
+        for (double &i : data) {
+            is >> i;
+        }
+        Eigen::Quaterniond q(data[6], data[3], data[4], data[5]);
+        q.normalize();
+        setMeasurement(Sophus::SE3(q, Eigen::Vector3d(data[0], data[1], data[2])));
+        for (int i = 0; i < information().rows() && is.good(); ++i) {
+            for (int j = i; j < information().cols() && is.good(); ++j) {
+                is >> information()(i, j);
+                if (i != j)
+                    information()(j, i) = information()(i, j);
+            }
+        }
+        return true;
+    }
+
+    bool write(std::ostream &os) const override {
+        auto *v1 = dynamic_cast<VertexSE3LieAlgebra *> (_vertices[0]);
+        auto *v2 = dynamic_cast<VertexSE3LieAlgebra *> (_vertices[0]);
+        os << v1->id() << " " << v2->id() << " ";
+        SE3 m = _measurement;
+        Eigen::Quaterniond q = m.unit_quaternion();
+        os << m.translation().transpose() << " ";
+        os << q.coeffs()[0] << " " << q.coeffs()[1] << " " << q.coeffs()[2] << " " << q.coeffs()[3] << " ";
+//        information matrix
+        for (int i = 0; i < information().rows(); ++i) {
+            for (int j = i; j < information().cols(); ++j) {
+                os << information()(i, j) << " ";
+            }
+        }
+        os << endl;
+        return true;
     }
 };
 
