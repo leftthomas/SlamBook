281b423d414cad51e3463868ab1faa6686d0de2d
left thomas <leftthomas@qq.com>
2017-08-30 19:56:50 +0800

    配置文件修改

--- ch11/pose_graph_g2o_se3.cpp
+++ ch11/pose_graph_g2o_se3.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
-#include <g2o/types>
+#include <g2o/core/base_vertex.h>
+#include <g2o/core/base_unary_edge.h>
+#include <g2o/core/block_solver.h>
+#include <g2o/core/optimization_algorithm_levenberg.h>
+#include <g2o/core/optimization_algorithm_gauss_newton.h>
+#include <g2o/core/optimization_algorithm_dogleg.h>
+#include <g2o/solvers/dense/linear_solver_dense.h>
 using namespace std;
 
 /**
