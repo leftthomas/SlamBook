6f94538d61d11b5b6de18f1a6e84b76d65b0097a
left thomas <leftthomas@qq.com>
2017-09-03 22:14:12 +0800

    完成DBoW3训练字典

--- ch12/feature_training.cpp
+++ ch12/feature_training.cpp
@@ -1,6 +1,9 @@
-#include<iostream>
+#include "DBoW3/DBoW3.h"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
 
 using namespace std;
+using namespace cv;
 
 
 /**
@@ -11,5 +14,31 @@ using namespace std;
  */
 int main(int argc, char **argv) {
 
+//    read the image
+    cout << "reading images." << endl;
+    vector<Mat> images;
+    for (int i = 0; i < 10; ++i) {
+        string path = "../../ch12/data/" + to_string(i + 1) + ".png";
+        images.push_back(imread(path));
+    }
+
+//    detect ORB features
+    cout << "detecting ORB features." << endl;
+    Ptr<Feature2D> detector = ORB::create();
+    vector<Mat> descriptors;
+    for (Mat &image:images) {
+        vector<KeyPoint> keypoints;
+        Mat descriptor;
+        detector->detectAndCompute(image, Mat(), keypoints, descriptor);
+        descriptors.push_back(descriptor);
+    }
+
+//    create vocabulary
+    cout << "creating vocabulary." << endl;
+    DBoW3::Vocabulary vocab;
+    vocab.create(descriptors);
+    cout << "vocabulary info: " << vocab << endl;
+    vocab.save("vocabulary.yml.gz");
+    cout << "done." << endl;
     return 0;
 }
