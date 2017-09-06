46d620b6a4a7b69c7836b02bd11e74f5f1a2e7c4
left thomas <leftthomas@qq.com>
2017-09-06 09:20:30 +0800

    单目稠密重建初始化

---
+++ ch13/dense_monocular.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+
+/**
+ * 本程序演示了单目稠密重建
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char **argv) {
+
+    return 0;
+}
