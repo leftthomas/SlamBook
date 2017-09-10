089ec73bd30b87daf6f2ca62e76e90a12d37636f
left thomas <leftthomas@qq.com>
2017-09-10 11:14:52 +0800

    八叉树地图初始化

---
+++ ch13/octomap_mapping.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <fstream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <octomap/octomap.h>
+#include <Eigen/Geometry>
+#include <boost/format.hpp>
+
+using namespace std;
+
+/**
+ * 本程序演示了octomap八叉树地图
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char **argv) {
+
+    return 0;
+}
